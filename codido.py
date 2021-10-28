61071ff5d2d36e8bbd76aca181acb70f09977642
rivera2004 <riberabarre99@gmail.com>
2021-10-27 22:00:10 -0500

    mi primer subida. Lines starting

---
+++ codido.py
@@ -0,0 +1,26 @@
+#buenos dias comenzamos con mi proyecto algoritmo concepto
+
+print("Fuente de algoritmo")
+
+# Me permitirira iniciar el programa que esta por    crear
+programa = input ("Concepto de algoritmos?")
+
+if programa == ("algoritmos"):
+ print("iniciando programa a realizar")
+ algortimo = input ("3.¿Cual seria el algoritmo que quisiera ingresar?")
+if algortimo == ("varios") :
+  i1 = ("1. rae_nocomputacionales")  
+  i2 = ("2. rae_cualitativos") 
+  i3 = ("3. rae_cuantitativos")
+  print(i1)
+  print(i2)
+  print(i3) 
+  if i1 == ("1") :
+    print ("Un algoritmo informático es una secuencia de instrucciones finitas que llevan a cabo una serie de procesos para dar respuesta a determinados problemas. Es decir, un algoritmo informático resuelve cualquier problema a través de unas instrucciones y reglas concisas, mostrando el resultado obtenido.")
+elif programa: 
+ print( "1.esto es falso la verdadera respues la encuentras en tu corazon -_-  XD. ")
+ print("2.vuelve a cargar la pagina para poder escribir")
+ print("https://www.facebook.com/marcos.ribera.733/")
+ print("https://www.instagram.com/ribera2401/")
+ print("+593 99 913 7276")
+
