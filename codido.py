fb29ed71199577cb01167ce2ebe4040719a6c1da
Antemp <96396558+DanielJesus23@users.noreply.github.com>
2022-09-28 20:10:35 +0200

    Aport Codec
    Ingrese una funcion para poder eligir los dias de las semana y aparte que me diga si es  un dia de trabajo o no
    
    Co-Authored-By: rivera2004 <93233621+rivera2004@users.noreply.github.com>

--- codido.py
+++ codido.py
@@ -1,26 +1,31 @@
-#buenos dias comenzamos con mi proyecto algoritmo concepto
 
-print("Fuente de algoritmo")
+def obtener_dia(numero_dia):
+    dias = {
+        0: "Domingo",
+        1: "Lunes",
+        2: "Martes",
+        3: "Miercoles",
+        4: "Jueves",
+        5: "Viernes",
+        6: "Sadabo",
+    }
+    return dias.get(numero_dia, "null")
 
-# Me permitirira iniciar el programa que esta por    crear
-programa = input ("Concepto de algoritmos?")
+disec = int(input("Ingrese un numero de los dias de la semana: "))
+print(obtener_dia(disec))
+if (disec > 5):
+        if(disec!=0):
+            print("Has elegido un dia de descando")
+else:
+    print("no es un dia de trabajo")    
 
-if programa == ("algoritmos"):
- print("iniciando programa a realizar")
- algortimo = input ("3.¿Cual seria el algoritmo que quisiera ingresar?")
-if algortimo == ("varios") :
-  i1 = ("1. rae_nocomputacionales")  
-  i2 = ("2. rae_cualitativos") 
-  i3 = ("3. rae_cuantitativos")
-  print(i1)
-  print(i2)
-  print(i3) 
-  if i1 == ("1") :
-    print ("Un algoritmo informático es una secuencia de instrucciones finitas que llevan a cabo una serie de procesos para dar respuesta a determinados problemas. Es decir, un algoritmo informático resuelve cualquier problema a través de unas instrucciones y reglas concisas, mostrando el resultado obtenido.")
-elif programa: 
- print( "1.esto es falso la verdadera respues la encuentras en tu corazon -_-  XD. ")
- print("2.vuelve a cargar la pagina para poder escribir")
- print("https://www.facebook.com/marcos.ribera.733/")
- print("https://www.instagram.com/ribera2401/")
- print("+593 99 913 7276")
+numero1 = int(input("Dígame un número: "))
+numero2 = int(input(f"Dígame un número mayor que {numero1}: "))
+print(f"La diferencia entre ellos es {numero2 - numero1}.")
 
+apert = (input("Ingrese su nombre: " ))
+print (f"Hola {apert} Tienes {numero2 - numero1} anios")
+
+#Ahora con numeros Reales
+numflo1 = Double(input("Ingrese un numero de un dia de la semana"))
+numflo2 = Double(input(f"Digite un numero de la semana diferente al {numflo1}"))
